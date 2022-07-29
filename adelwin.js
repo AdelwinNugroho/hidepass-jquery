f0ea88bdd59ac076d077af8b89a9088af4914b3c
Adelwin Nugroho Liandy <100504259+AdelwinNugroho@users.noreply.github.com>
2022-07-29 15:14:20 +0700

    Add files via upload

---
+++ adelwin.js
@@ -0,0 +1,19 @@
+	$(document).ready(function() {
+	  var cek = $('.form-checkbox').val();
+	  $('.form-checkbox').click(function() {
+	    if ($(this).is(':checked')) {
+	      $('.form-password').attr('type', 'text');
+	    } else {
+	      $('.form-password').attr('type', 'password');
+	    }
+	  });
+	});	$(document).ready(function() {
+	  var cek = $('.form-checkbox').val();
+	  $('.form-checkbox').click(function() {
+	    if ($(this).is(':checked')) {
+	      $('.form-password').attr('type', 'text');
+	    } else {
+	      $('.form-password').attr('type', 'password');
+	    }
+	  });
+	});
